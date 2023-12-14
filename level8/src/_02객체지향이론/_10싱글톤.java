f8d4ac868294654d564680ae24e4e4df781214b6
sinhyeongi <106128885+sinhyeongi@users.noreply.github.com>
2023-12-14 11:41:16 +0900

    Delete level8 directory

--- level8/src/_02객체지향이론/_10싱글톤.java
+++
@@ -1,9 +0,0 @@
-package _02객체지향이론;
-
-public class _10싱글톤 {
-
-	public static void main(String[] args) {
-		
-	}
-
-}
--- level8/src/콜랙션백터_이론/arraylist.java
+++
@@ -1,9 +0,0 @@
-package 콜랙션백터_이론;
-
-public class arraylist {
-
-	public static void main(String[] args) {
-
-	}
-
-}
